03a9d03a5c70b02bcce3090b25ca9594d6668a0e
brianlmerritt <brianlmerritt@noreply.com>
2024-10-25 10:11:06 +0100

    bump version

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016100700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2024101701;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;        // Requires this Moodle version
 $plugin->component = 'message_ombiel_alerts';  // Full name of the plugin (used for diagnostics)
